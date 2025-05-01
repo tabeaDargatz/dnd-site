668160bad09e38670c67d6b54a5fffe68efb5a68
Tabea <dargatztabea@gmail.com>
2025-05-01 21:19:53 +0200

    initial character creation done

---
+++ public/createChar.js
@@ -0,0 +1,25 @@
+import "https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js";
+
+window.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("save").addEventListener("click", () => {
+    let updateData = new Object();
+    updateData["name"] = document.getElementById("name").value;
+    updateData["campaign"] = document.getElementById("campaign").value;
+
+    axios
+      .post(
+        `https://discord-dice-roll-bot.dargatztabea.workers.dev/api/create`,
+        JSON.stringify(updateData)
+      )
+      .then((response) => {
+        console.log(response);
+        if ((response.status = 200)) {
+          alert("Character created successfully!");
+        } else {
+          alert("Something went wrong. Im gonna kill myself.");
+          console.log(response);
+        }
+      })
+      .catch((error) => this.alert(error));
+  });
+});
